d0672c55e6dfe81668ec009b878f3d69d279eb51
oumouyacouba <oumouyacouba1234@gmail.com>
2026-06-14 17:52:48 +0000

    Add multiplication table generator using loops

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const btn = document.getElementById("btn")
 const result = document.getElementById("result")
 // verifier si le bouton marche//
 btn.onclick = function(){
+//lire le input value=nombre que tu tapes//
 let inputnumber = number.value
 result.textContent = inputnumber;
+//le boucle 
+let texte = "" //feuille vide pour stocker,ecrire les trucs//
+for (let i = 1; i <= 10; i++) {
+    texte += inputnumber + "X" + i + "=" + (inputnumber * i) + "<br>";
+    result.innerHTML = texte; // afficher UNE seule fois
+}
+//
 }
