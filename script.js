34c08f30c25a5168531f0889226b0aa9da499d14
oumouyacouba <oumouyacouba1234@gmail.com>
2026-06-14 17:03:15 +0000

    add onclick function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let number = document.getElementById("number")
 const btn = document.getElementById("btn")
 const result = document.getElementById("result")
-btn.onclick =
+btn.onclick = function(){
+    
+}
