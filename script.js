9c2dcbfe9eff98c8531babb27ee0df138630ac0c
oumouyacouba <oumouyacouba1234@gmail.com>
2026-06-14 17:17:27 +0000

    get input value

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let number = document.getElementById("number")
 const btn = document.getElementById("btn")
 const result = document.getElementById("result")
+// verifier si le bouton marche//
 btn.onclick = function(){
-    
+let inputnumber = number.value
+result.textContent = inputnumber;
 }
